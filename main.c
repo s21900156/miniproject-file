015198f2a775f5e4567362cc196f3552439dcc8c
s21900156 <21900156@handong.ac.kr>
2022-04-18 23:43:17 +0900

    9commit

--- main.c
+++ main.c
@@ -5,9 +5,8 @@
 #include "manager.h"
 
 int main(void){
-#ifdef DEBUG
     printf("***DEBUG: %s %s %s %d\n", __DATE__,__TIME__,__FILE__,__LINE__);
-#endif
+
     Product plist[20];
     int index =0;
     int count = 0, menu;
